1098abb99b39a3d9757b08eb78446a77ba39a9f4
Jivesh-2231 <jivesh2231@gmail.com>
2025-07-30 09:57:05 +0530

    Add files via upload

---
+++ First_class/src/Math_Ops.java
@@ -0,0 +1,28 @@
+
+public class Math_Ops {
+	public void add() {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);		
+	}
+	public void sub() {
+		int a = 10;
+		int b = 20;
+		int c = a-b;
+		System.out.println(c);
+	}
+	public void mul() {
+		int a = 10;
+		int b = 20;
+		int c = a*b;
+		System.out.println(c);
+	}
+	public void div() {
+		int a = 10;
+		int b = 20;
+		int c = a/b;
+		System.out.println(c);
+	}
+
+}
---
+++ First_class/src/Testr.java
@@ -0,0 +1,11 @@
+
+public class Testr {
+	public static void main(String[] args) {
+		Math_Ops obj = new Math_Ops();
+		obj.add();
+		obj.sub();
+		obj.mul();
+		obj.div();
+	}
+
+}
